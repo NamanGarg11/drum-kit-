f2ab64c87041697da3fcb982176fba407bc4f26e
NAMAN <namangarg09810@gmail.com>
2024-10-13 23:58:57 +0530

    this is just a basic project

---
+++ index.js
@@ -0,0 +1,54 @@
+var number = document.querySelectorAll(".drum").length;
+for(var i=0;i<number;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+       var buttoninnerHtml = this.innerHTML;
+       makesound(buttoninnerHtml);
+       buttonanimation(buttoninnerHtml);
+
+    })
+}
+addEventListener("keydown",function(yes){
+    makesound(yes.key);
+    buttonanimation(yes.key);
+})
+function makesound(key){
+    switch(key){
+        case 'w': 
+        var crash= new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+        case "a":
+        var kick= new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1= new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2= new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3= new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4= new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+         default: console.log(buttoninnerHtml);
+        }
+}
+function buttonanimation(key){
+    var button = document.querySelector("."+key);
+    button.classList.add("pressed");
+    setTimeout(function(){
+        button.classList.remove("pressed");
+        },100) ;
+
+} 
